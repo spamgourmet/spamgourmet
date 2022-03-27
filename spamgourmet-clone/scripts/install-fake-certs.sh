9b508c68d2ef467f74cb9a85303b3146316ea483
Michael De La Rue <tiggmal.mddlr@9ox.net>
2022-03-27 16:39:35 +0200

    make a draft of fake certs using openssl

--- spamgourmet-clone/scripts/install-fake-certs.sh
+++ spamgourmet-clone/scripts/install-fake-certs.sh
@@ -19,11 +19,31 @@ source sg-server-config.sh
 ###
 ##########################################################################
 
-[ -e /etc/ssl/private/dkim.pem ]    && exit
-[ -e /etc/ssl/private/$DOMAIN.pem ] && exit
+DOMAIN=spamgourmet.test
 
-### change this to generate real certs
+DKIM_KEY_FILE=/etc/ssl/private/dkim.pem
+DKIM_DNS_FILE=/etc/ssl/certs/dkim_dns_data.txt
+DOMAIN_KEY_FILE=/etc/ssl/private/$DOMAIN.pem
+DOMAIN_CERT_FILE=/etc/ssl/certs/$DOMAIN.crt
+
+
+if [ -e "$DKIM_KEY_FILE" -o -e "$DOMAIN_KEY_FILE" ]
+then
+   echo "Aborting since key file already exists" >&2
+   ls -l /etc/ssl/private > &2 
+   exit 5
+fi
+
+
+# after https://stackoverflow.com/questions/10175812/how-to-generate-a-self-signed-ssl-certificate-using-openssl
+openssl req -subj "/CN=$TEST_DOMAIN" -x509 -newkey rsa:4096 -keyout "$DOMAIN_KEY_FILE" -out "$DOMAIN_CERT_FILE" -sha256 -days 365
+
+# after https://www.mailhardener.com/kb/how-to-create-a-dkim-record-with-openssl
+openssl genrsa -out "$DKIM_KEY_FILE" 2048
+openssl rsa -in "$DKIM_KEY_FILE" -pubout -outform der 2>/dev/null | openssl base64 -A > "$DKIM_DNS_FILE"
+
+
 touch /etc/ssl/private/dkim.pem;    chmod 0600 /etc/ssl/private/dkim.pem
 touch /etc/ssl/certs/dkim.crt;      chmod 0644 /etc/ssl/certs/dkim.crt
 touch /etc/ssl/private/$DOMAIN.pem; chmod 0600 /etc/ssl/private/$DOMAIN.pem
-touch /etc/ssl/certs/$DOMAIN.crt;   chmod 0644 /etc/ssl/certs/$DOMAIN.crt
+touch /etc/ssl/certs/$DOMAIN.crt;   chmod 0644 
