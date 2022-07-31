c2409902f6de43550897d0233f0b59b2a481b74f
josiah-hamilton <github.josiah@xoxy.net>
2022-07-31 00:47:23 +0000

    add AlternativeDeliveryAddresses and LookUp tables

--- conf/db.sql
+++ conf/db.sql
@@ -16,23 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `AddressAudit`
---
-
-DROP TABLE IF EXISTS `AddressAudit`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `AddressAudit` (
-  `AddressAuditID` int(11) NOT NULL AUTO_INCREMENT,
-  `UserID` int(11) NOT NULL DEFAULT '0',
-  `Address` varchar(255) DEFAULT NULL,
-  `ChangeTime` int(11) NOT NULL DEFAULT '0',
-  `IPAddress` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`AddressAuditID`)
-) ENGINE=MyISAM AUTO_INCREMENT=513704 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `AdminEmail`
 --
 
