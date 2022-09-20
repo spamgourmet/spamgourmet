57a11e554f5761bd8989cab78e86bd8e898dd3a1
Michael De La Rue <tiggmal.mddlr@9ox.net>
2022-09-20 17:08:49 +0100

    Make tests that actually run spameater

--- test/test-maileater.sh
+++ test/test-maileater.sh
@@ -1,5 +1,49 @@
-testMailEaterShouldAcceptMaail () {
-    assertEquals 1 2
+oneTimeSetUp () {
+    #FIXME - change the way we handle path substitution and then get
+    #rid of this. Bad because it forces test to run in docker container
+    mkdir /path
+    if [ ! -e /path/to ]
+    then
+        ln -s $PWD/conf /path/to
+    fi
+    mv /usr/sbin/sendmail /usr/sbin/sendmail.disabled
+
+    if [ -e sendmail ]
+    then
+        echo "sendmail not disabled ; aborting"
+        exit 9
+    else
+        ( echo '#!/bin/bash'
+          echo 'cat >> $SENDMAIL_OUT' ) > /usr/sbin/sendmail
+        chmod +x /usr/sbin/sendmail
+    fi
+}
+
+oneTimeTeardown () {
+    rm /path/to
+    rmdir /path
+    mv /usr/sbin/sendmail.disabled /usr/sbin/sendmail 
+
+}
+
+testMailEaterShouldRejectBadMail () {
+    SENDMAIL_OUT=`tempfile -d $SHUNIT_TMPDIR`
+    #
+    #FIXME start stuff for spameater
+
+    /usr/bin/perl -Imodules -s mailhandler/spameater  -extradebug=5 -debugstderr=5 <   test/fixture/reject_wrong_domain.email
+
+    assertFalse "incorrect mail accepted" '[ -s SENDMAIL_OUT ]'
+}
+
+testMailEaterShouldAcceptMail () {
+    SENDMAIL_OUT=`tempfile -d $SHUNIT_TMPDIR`
+    #
+    #FIXME start stuff for spameater
+
+    /usr/bin/perl -Imodules  -s mailhandler/spameater  -extradebug=5 -debugstderr=5 <   test/fixture/accept_very_simple.email
+
+    assertTrue "valid mail not accepted" '[ -s SENDMAIL_OUT ]'
 }
 
 # Load shUnit2
