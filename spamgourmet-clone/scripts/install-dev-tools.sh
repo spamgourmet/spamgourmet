00434d6275c59284a64765a274eef9de43fc950b
Michael De La Rue <tiggmal.mddlr@9ox.net>
2022-10-05 13:40:08 +0100

    shell script automated formatting fixes from pre-commit / prettier

--- spamgourmet-clone/scripts/install-dev-tools.sh
+++ spamgourmet-clone/scripts/install-dev-tools.sh
@@ -6,11 +6,13 @@ source sg-server-config.sh
 cd $SCRIPT_BASE_DIR
 
 function development_packages {
-	echo '##########################################################################'
-	echo '### install mandatory packages'
-	echo '##########################################################################'
-	apt-get update; apt-get upgrade -y; apt-get autoremove
-	apt-get install -y shunit2
+  echo '##########################################################################'
+  echo '### install mandatory packages'
+  echo '##########################################################################'
+  apt-get update
+  apt-get upgrade -y
+  apt-get autoremove
+  apt-get install -y shunit2
 }
 
 development_packages
